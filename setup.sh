8c9356dbbb5976274c7a923f45305908b80e1311
Shivakar Vulli <shivakar@users.noreply.github.com>
2023-08-13 16:50:30 -0400

    Update setup (#5)
    * update gitignore
    * Fix gpg-connect-agent mangling terminals
    * Fixes path to pinentry-curses
    * Fixes MacOS setup to fully automated
    * Update brew installs

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 if [ "${PWD}" != "$HOME/bin" ]; then
     echo "PWD != $HOME/bin."
@@ -31,27 +31,49 @@ then
 	go install github.com/junegunn/fzf@latest
 elif [[ "$(uname)" == "Darwin" ]];
 then
-	echo -n "Installing package from Brewfile."
-	brew bundle install --file=Brewfile
+	echo -n "Installing Rosetta"
+	softwareupdate --install-rosetta --agree-to-license
+
+	if [ ! -f /opt/homebrew/bin/brew ]; then
+		echo -n "Installing homebrew"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+
+	if [ ! -f /opt/homebrew/bin/brew ]; then
+		echo -n "Homebrew installation was not successful. Exiting..."
+		exit 3
+	fi
+
+
+	echo -n "Installing packages from Brewfile."
+	/opt/homebrew/bin/brew bundle install --file=Brewfile
+
+	export PATH=/opt/homebrew/bin:$PATH
+	sudo ln -sfn /opt/homebrew/bin/pinentry-curses /usr/local/bin
 fi
+
+if command -v op &> /dev/null; then
+	op signin &> /dev/null
+	if [[ "$?" != "0" ]]; then
+		echo "Please link 1password CLI and rerun setup.sh"
+		exit 4
+	fi
+else
+	echo "Please install 1Password CLI and rerun setup.sh"
+	exit 5
+fi
+
 
 ## Setup symlinks to dotfiles
 
 ### zsh config
 echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 ln -sf ~/bin/zsh/zshrc ~/.zshrc
 ln -sf ~/bin/zsh/inputrc ~/.inputrc
 touch ~/.localbashrc
 
-### ssh config
-ln -sfn ~/bin/ssh/ ~/.ssh
-
-### gnupg config
-ln -sfn ~/bin/gnupg ~/.gnupg
-
-### git config
-ln -sf ~/bin/git/gitconfig ~/.gitconfig
+op document get p10k.zsh --out-file ~/.p10k.zsh --force
 
 #### tmux config
 ln -sfn ~/bin/tmux ~/.tmux
@@ -64,6 +86,7 @@ mkdir -p ~/.tmux/plugins
 ln -sfn ~/bin/vim ~/.vim
 type vim &>/dev/null && vim +PlugInstall +PlugClean +qall
 
+
 ### Setup TouchID for sudo
 if  [[ "$(uname)" == "Darwin" ]];
 then
@@ -71,5 +94,31 @@ then
 	    (echo "Setting up TouchID for sudo" && sudo gsed -i '2 i auth       sufficient     pam_tid.so' /etc/pam.d/sudo)
 fi
 
+### ssh config
+ln -sfn ~/bin/ssh/ ~/.ssh
+op document get id_rsa.pub --out-file ~/.ssh/id_rsa.pub --force
+op document get id_rsa --out-file ~/.ssh/id_rsa --force
+op document get id_ed25519_cardno_22275657.pub --out-file ~/.ssh/id_ed25519_cardno_22275657.pub --force
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+chmod 400 ~/.ssh/*.pub
+chmod 400 ~/.ssh/id_rsa
 
+
+### gnupg config
+ln -sfn ~/bin/gnupg ~/.gnupg
+chmod 700 ~/.gnupg
+op document get master.pub.key --out-file ~/.gnupg/master.pub.key --force
+op document get ownertrust.txt --out-file ~/.gnupg/ownertrust.txt --force
+gpg --import < ~/.gnupg/master.pub.key
+gpg --import-ownertrust < ~/.gnupg/ownertrust.txt
+
+### git config
+ln -sf ~/bin/git/gitconfig ~/.gitconfig
+
+### Final instructions
+echo ""
 echo "dotfiles setup complete..."
+echo "Restart the terminal and then do the following:"
+echo "  1. Run `p10k configure` to install missing fonts and enable iterm2 integration."
+
